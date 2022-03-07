95e89dcb023977c89c45c63750dd919706ed055a
James-S-B <jamessbenerofe23@gmail.com>
2022-03-07 10:19:18 -0800

    generate two way temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,22 @@
-const ftoc = function() {
+const ftoc = function (fTemp) {
+  let temp = fTemp - 32;
+  temp = (temp * 5) / 9;
+  temp = Number(temp.toFixed(1));
 
+  return temp;
 };
 
-const ctof = function() {
+const ctof = function (cTemp) {
+  return Number(((cTemp * 9) / 5 + 32).toFixed(1));
+  /* let temp = (cTemp * 9) / 5;
+  temp = temp + 32;
+  temp = Number(temp.toFixed(1));
 
+  return temp; */
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
