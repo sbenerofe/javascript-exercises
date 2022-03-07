dfc81d4810f0359c49649574e804046aca570b38
James-S-B <jamessbenerofe23@gmail.com>
2022-03-07 09:18:52 -0800

    fix array remove function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...remove) {
+  console.log(remove);
+  remove.forEach((element) => {
+    if (array.includes(element)) {
+      array.splice(array.indexOf(element), 1);
+    }
+  });
+  return array;
 };
 
 // Do not edit below this line
