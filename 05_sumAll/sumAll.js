a9157f17c7ea22451a8bca7c1aa9a3a263b6383c
James-S-B <jamessbenerofe23@gmail.com>
2022-03-07 09:43:18 -0800

    fix sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  if (
+    typeof firstNumber === "number" &&
+    typeof secondNumber === "number" &&
+    firstNumber >= 0 &&
+    secondNumber >= 0
+  ) {
+    if (firstNumber > secondNumber) {
+      console.log(firstNumber, secondNumber);
+      [firstNumber, secondNumber] = [secondNumber, firstNumber];
+      console.log(firstNumber, secondNumber);
+    }
+    let sum = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
