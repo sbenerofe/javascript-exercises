e4c698dd2a03f6ce5fe8e0e48745ac4478c83690
Steven Benerofe <sbenerofe@gmail.com>
2022-03-09 18:17:57 -0800

    fix reversing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+  const f = inputString
+    .replaceAll(" ", "")
+    .replaceAll(",", "")
+    .replaceAll(".", "")
+    .replaceAll("!", "")
+    .toLowerCase();
 
+  const wreverse = f.split("").reverse().join("");
+  return f === wreverse;
 };
 
 // Do not edit below this line
