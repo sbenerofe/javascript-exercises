28068356839a7a92f5e03785bca2b6ad8de36b06
Steven Benerofe <sbenerofe@gmail.com>
2022-03-09 18:36:16 -0800

    update fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (term) {
+  const number = Number(term);
+  if (number < 0) {
+    return "OOPS";
+  }
+  if (number < 3) {
+    return 1;
+  }
+  let currentTerm = 1;
+  let previousTerm1Back = 1;
+  let previousTerm2Back = 1;
+  for (let i = 3; i <= number; i++) {
+    previousTerm2Back = previousTerm1Back;
+    previousTerm1Back = currentTerm;
+    currentTerm = previousTerm2Back + previousTerm1Back;
+  }
+  return currentTerm;
 };
 
 // Do not edit below this line
